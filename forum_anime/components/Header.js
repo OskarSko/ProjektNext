f012c75bd15f54c819ac355d8fd14e52df7f94c9
OskarSko <skowronskioskar372@gmail.com>
2025-02-08 18:58:55 +0100

    nav

--- forum_anime/components/Header.js
+++ forum_anime/components/Header.js
@@ -3,7 +3,6 @@ import Nav from './Nav';
 export default function Header() {
   return (
     <header className="header">
-      <h1>Anime Forum</h1>
       <Nav />
     </header>
   );
--- forum_anime/components/Nav.js
+++ forum_anime/components/Nav.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
+import styles from './Nav.module.css'; // Upewnij się, że plik CSS jest poprawny
 
 export default function Nav() {
   return (
-    <nav className="nav">
-      <ul>
-        <li><Link href="/anime">Anime</Link></li>
-        <li><Link href="/kontakt">Kontakt</Link></li>
-        <li><Link href="/forum">forum</Link></li>
-      </ul>
-    </nav>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">Anime Forum</Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li><Link href="/anime">Anime</Link></li>
+          <li><Link href="/kontakt">Kontakt</Link></li>
+          <li><Link href="/forum">Forum</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
